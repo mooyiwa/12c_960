d9411a3673f84682f947d8407f2099771f11b4b3
silasb <sdelesil@gmail.com>
2016-04-12 18:09:31 +0100

    Ïnitial commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <title>12c CSS Framework</title>
+
+    <link href="12c_960.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <!-- declare a wrapper class of 'container' -->
+      <div class="container">
+          
+          <h1 style="text-align:center;margin-bottom: 20px; margin-top: 20px">12c Fixed Grid CSS Framework -- 960px wide</h1>
+          <!-- 'alpha' class defined on every new row -->
+          
+              <div class="grid_12 alpha">
+                  A 12 column grid
+              </div>
+          
+              <div class="grid_1 alpha">
+                  A 1 col grid
+              </div>
+              <div class="grid_11">
+                  An 11 col grid
+              </div>
+          
+              <div class="grid_2 alpha">
+                  A 2 col grid
+              </div>
+              <div class="grid_10">
+                  A 10 col grid
+              </div>
+          
+             <div class="grid_3 alpha">
+                  A 3 col grid
+              </div>
+              <div class="grid_9">
+                  A 9 col grid
+              </div>
+          
+              <div class="grid_4 alpha">
+                  A 4 col grid
+              </div>
+              <div class="grid_8">
+                  An 8 col grid
+              </div>
+          
+              <div class="grid_5 alpha">
+                  A 5 col grid
+              </div>
+              <div class="grid_7">
+                  A 7 col grid
+              </div>
+          
+              <div class="grid_6 alpha">
+                  A 6 col grid
+              </div>
+              <div class="grid_6">
+                  A 6 col grid
+              </div>
+          
+              <div class="grid_7 alpha">
+                  A 7 col grid
+              </div>
+              <div class="grid_5">
+                  A 5 col grid
+              </div>
+          
+              <div class="grid_8 alpha">
+                  An 8 col grid
+              </div>
+              <div class="grid_4">
+                  A 4 col grid
+              </div>
+          
+              <div class="grid_9 alpha">
+                  A 9 col grid
+              </div>
+              <div class="grid_3">
+                  A 3 col grid
+              </div>
+          
+              <div class="grid_10 alpha">
+                  A 10 col grid
+              </div>
+              <div class="grid_2">
+                  A 2 col grid
+              </div>
+          
+              <div class="grid_11 alpha">
+                  An 11 col grid
+              </div>
+              <div class="grid_1">
+                  A 1 col grid
+              </div>
+  
+          </div>
+      </div>
+
+
+  </body>
+</html>
